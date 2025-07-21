febe80fbe54cf744c21fd17057e97ec9fb95b363
Alzubair98 <zubairsk53@gmail.com>
2025-07-21 12:13:56 +0300

    update code

--- src/stores/useEmail.js
+++ src/stores/useEmail.js
@@ -52,10 +52,13 @@ export const useEmailStore = defineStore('email', () => {
   const sendMail = async () => {
     const payload = {
       subject: mailData.value.subject,
-      body: mailData.value.body,
-      email_1: mailData.value.template,
-      name: mailData.value.name,
-      phone: mailData.value.phoneNumber,
+      body: {
+        subject: mailData.value.subject,
+        text: mailData.value.body,
+        email: mailData.value.template,
+        name: mailData.value.name,
+        phone: mailData.value.phoneNumber,
+      },
     }
 
     try {
